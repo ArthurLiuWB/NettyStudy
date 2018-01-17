e9ba3c3a5ba25a6c88ee7aa9b7eacf57377ce66b
ArthurLiuWB <812422725@qq.com>
2018-01-17 20:17:17 +0800

    Initial commit

---
+++ src/main/java/cn/wenbo/App.java
@@ -0,0 +1,13 @@
+package cn.wenbo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
